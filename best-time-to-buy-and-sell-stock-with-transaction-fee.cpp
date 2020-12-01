7d1e888e43b9d749bace26f6d0040f2bd5d9b7c0
Preetham Dasari <preethamdasari@gmail.com>
2020-12-01 14:45:20 -0500

    working commit - Created using LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int days = prices.size();
+        if(days==0){return 0;}
+        vector<int> hasStock(days+1,0);
+        vector<int> hasNoStock(days+1,0);
+        
+        //Base Case Initialization
+        hasNoStock[0] = 0;
+        hasStock[0] = -prices[0];
+        
+        for(int i=1;i<days;i++){
+            hasNoStock[i] = max(hasNoStock[i-1],hasStock[i-1]+prices[i]-fee);
+            hasStock[i] = max(hasStock[i-1],hasNoStock[i-1]-prices[i]);
+        }
+        return hasNoStock[days-1];
+    }
+};

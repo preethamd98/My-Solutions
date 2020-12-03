79f77b92090b0bf9252b205098aeeee2939da3d9
Preetham Dasari <preethamdasari@gmail.com>
2020-12-02 21:23:04 -0500

    Update split-a-string-in-balanced-strings.cpp

--- split-a-string-in-balanced-strings.cpp
+++ split-a-string-in-balanced-strings.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int balancedStringSplit(string s) {
-        int u = 0;
-        int l = 0;
-        int counter = 0;
-        for(char c:s){
-            if(c=='R'){u++;}
-            else{l++;}
-            if(l==u){counter++;}
-        }
-        return counter;
-    }
+    int balancedStringSplit(string s) {
+        int r = 0;
+        int l = 0;
+        int counter = 0;
+        for(char c:s){
+            if(c=='R'){r++;}
+            else{l++;}
+            if(l==r){counter++;}
+        }
+        return counter;
+    }
 };

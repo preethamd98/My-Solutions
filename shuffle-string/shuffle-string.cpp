48f729c6a1d2e597408bbfc94561f176aec92b94
Preetham Dasari <preethamdasari@gmail.com>
2020-12-07 16:06:34 -0500

    Update shuffle-string.cpp

--- shuffle-string/shuffle-string.cpp
+++ shuffle-string/shuffle-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    string restoreString(string s, vector<int>& indices) {
-        map<int,char> M;
-        for(int i=0;i<s.size();i++){
-            M[indices[i]] = s[i];
-        }
-        string ss = "";
-        for(auto it:M){
-            ss+=it.second;
-        }
-        return ss;
-    }
+    string restoreString(string s, vector<int>& indices) {
+        unordered_map<int,char> M;
+        for(int i=0;i<s.size();i++){
+            M[indices[i]] = s[i];
+        }
+        string ss = "";
+        for(int i=0;i<s.size();i++){
+            ss+=M[i];
+        }
+        return ss;
+    }
 };

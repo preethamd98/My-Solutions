a250c8d5f8ece43a39e5381d8aac030d886eb9b2
Preetham Dasari <preethamdasari@gmail.com>
2020-12-28 01:16:17 -0500

    Time: 24 ms (40.82%), Space: 13.3 MB (22.45%) - LeetHub

---
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string a, string b) {
+        int size_a = a.size();
+        int size_b = b.size();
+        if(size_a==0 || size_b==0){return 0;}
+        vector<vector<int>> dp(size_a + 1, vector<int>(size_b + 1, 0));
+        for (int i = 1; i <= size_a; i++) {
+            for (int j = 1; j <= size_b; j++) {
+                if (a[i-1] == b[j-1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[size_a][size_b];
+    }
+};

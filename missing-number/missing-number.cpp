a18c9b569ab9932017a9dad3f7d1081b25bd6f87
Preetham Dasari <preethamdasari@gmail.com>
2021-07-27 17:40:05 -0400

    Time: 20 ms (54.33%), Space: 18 MB (61.53%) - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    int left = 0, right = nums.size(), mid=(left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) right = mid;
+        else left = mid+1;
+    }
+    return left;
+    }
+};

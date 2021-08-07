c0c292efff42b86eacb551aabe43b255a44e8f9b
Preetham Dasari <preethamdasari@gmail.com>
2021-08-07 13:36:47 -0400

    Time: 8 ms (59.72%), Space: 8.7 MB (10.01%) - LeetHub

---
+++ design-hit-counter/design-hit-counter.cpp
@@ -0,0 +1,31 @@
+class HitCounter {
+public:
+    unordered_map<int,int> M;
+    /** Initialize your data structure here. */
+    HitCounter() {
+        
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        M[timestamp]++;
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        int counter = 0;
+        for(int i=timestamp-299;i<=timestamp;i++){
+            counter += M[i];
+        }
+        return counter;
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
